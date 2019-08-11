6fd0adc4139ad83e91e264e29efde2ddda87e1cf
adil <adeekerimov@gmail.com>
2019-08-11 19:37:10 +0600

    Initial project, outlet collections

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Adil on 8/11/19.
+//  Copyright © 2019 Adil & Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Chosen card was not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
+}
+
